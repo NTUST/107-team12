95b0305082326b0b44c69f331342ecc2480d0270
ClaireSang <claire0230025@gmail.com>
2018-05-30 20:15:26 +0800

    0530
    0530

---
+++ web/ntust/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\claire\documents\github\107-team12\web\ntust\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
